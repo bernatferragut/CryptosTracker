70265b42d2d3ba38c538e229fd725f40943a8466
elbernat <elbernat@gmail.com>
2018-04-08 17:11:09 -0400

    Adding code plan

---
+++ src/plan.js
@@ -0,0 +1,13 @@
+// 1. Create a Class that defines the Crypyo - we will have different cryptos so a class will work well
+// 1.1 Parameteres can be: name, symbol, hodl, price, amount, total USD, total BTC ... etc
+
+// 2. Create a Wallet Object which holds a Cryptos objects list and different Methods
+// 2.1 Methods can be: add crypto, delete crypto, create wallet, delete wallet...etc
+
+// 3. Am object that handles the different button actions can be created to keep the code tidy
+
+// 4. Interface with a form made of inputs that will create the different crypto objects added to the wallet list.
+
+// 5. Fetch the data from the crypto object from Cryptocompare website realtime data.
+
+// 6. A first version in vanilla javascript with CSS GRID and a second with React components are the aims
