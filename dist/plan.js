9a43f9e8f00bd9b4a5f1cf3809a70334626aa701
elbernat <elbernat@gmail.com>
2018-04-10 19:27:44 -0400

    Cleaning code
    Refactoring and testing 1

--- dist/plan.js
+++ dist/plan.js
@@ -1,25 +1,39 @@
 
-// 0. Create an combo input or simply an imput with validation + Input number + Add button
-let inputCrypto = document.getElementById('inputCrypto');
-
-// ADD CRYPTOS
-let nameOfCrypto='';
-
-function addCrypto() {
-    let inputValue = inputCrypto.value;
-    nameOfCrypto = String(inputValue).toUpperCase();
-    console.log(nameOfCrypto);
-    return nameOfCrypto;
+// CRYPTO CLASS
+class Crypto {
+    constructor(inputSymbol, inputAmount){
+        this.symbol = inputSymbol;
+        this.amount = inputAmount;
+    }
 }
 
-// HODLS OBJECT
-let hodl = {
-    BTC : {symbol: "BTC", amount: 1000}
+// ADD CRYPTOS
+let inputSymbol ="";
+let inputAmount = "";
+let cryptosList = [];
+
+function addCrypto() {
+    // input data taken ( a string and a number )
+    let inputSymbol = String(document.getElementById('symbolCrypto').value).toUpperCase();
+    let inputAmount = Number(document.getElementById('amountCrypto').value);
+    // crypto creation
+    let cryptoAdded = new Crypto(inputSymbol, inputAmount);
+    // Added to the cryptolist
+    cryptosList.push(cryptoAdded);
+    // consoles
+    console.log(`Crypto added: ${cryptoAdded.symbol}`);
+    console.log(`Amount added: ${cryptoAdded.amount}`);
+    console.log(cryptoAdded);
+    console.log(cryptosList);
+    console.log(cryptosList[0]);
+    // clean inputs
+    inputSymbol.value = '';
+    inputAmount.value = '';
 }
 
 // GET API DATA
 function getCryptos() {
-    let cryptosUrl = `https://min-api.cryptocompare.com/data/price?fsym=${nameOfCrypto}&tsyms=USD`;
+    let cryptosUrl = `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${cryptosList[0].symbol}&tsyms=USD`;
     let theResponse;
     fetch(cryptosUrl)
         .then(blob => blob.json()) 
@@ -31,24 +45,3 @@ function getCryptos() {
         }) // Second Promise as real response
         .catch(err => console.log('There was an error', err)); // In case of Error
 }
-
-
-
-// Collect this information into one new object( { crypto: name, hodl: amount }) from crypto class and add them to a list
-// Once all the Cryptos have been added we click the create Wallet Button which makes the call
-// to the 'generated address' with the values selected
-
-// 1. Create a Class that defines the Crypyo - we will have different cryptos so a class will work well
-// 1.1 Parameteres can be: name, symbol, hodl, price, amount, total USD, total BTC ... etc
-
-// 2. Create a Wallet Object which holds a Cryptos objects list and different Methods
-// 2.1 Methods can be: add crypto, delete crypto, create wallet, delete wallet...etc
-
-// 3. Am object that handles the different button actions can be created to keep the code tidy
-
-// 4. Interface with a form made of inputs that will create the different crypto objects added to the wallet list.
-
-// 5. Fetch the data from the crypto object from Cryptocompare website realtime data.
-
-// 6. A first version in vanilla javascript with CSS GRID and a second with React components are the aims
-
