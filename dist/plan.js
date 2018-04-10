57020ce64e61768a3bfae51a308eacacd1880646
elbernat <elbernat@gmail.com>
2018-04-09 20:07:35 -0400

    First input working and call to API
    First input working and call to API

--- dist/plan.js
+++ dist/plan.js
@@ -1,5 +1,39 @@
 
 // 0. Create an combo input or simply an imput with validation + Input number + Add button
+let inputCrypto = document.getElementById('inputCrypto');
+
+// ADD CRYPTOS
+let nameOfCrypto='';
+
+function addCrypto() {
+    let inputValue = inputCrypto.value;
+    nameOfCrypto = String(inputValue).toUpperCase();
+    console.log(nameOfCrypto);
+    return nameOfCrypto;
+}
+
+// HODLS OBJECT
+let hodl = {
+    BTC : {symbol: "BTC", amount: 1000}
+}
+
+// GET API DATA
+function getCryptos() {
+    let cryptosUrl = `https://min-api.cryptocompare.com/data/price?fsym=${nameOfCrypto}&tsyms=USD`;
+    let theResponse;
+    fetch(cryptosUrl)
+        .then(blob => blob.json()) 
+        .then((res) => {
+            let theResponse = res;
+            // Visualize Data
+            let blobData = JSON.stringify(theResponse, null, 2);
+            data.innerHTML = blobData;
+        }) // Second Promise as real response
+        .catch(err => console.log('There was an error', err)); // In case of Error
+}
+
+
+
 // Collect this information into one new object( { crypto: name, hodl: amount }) from crypto class and add them to a list
 // Once all the Cryptos have been added we click the create Wallet Button which makes the call
 // to the 'generated address' with the values selected
@@ -18,58 +52,3 @@
 
 // 6. A first version in vanilla javascript with CSS GRID and a second with React components are the aims
 
-
-
-// HODLS OBJECT
-let hodl = {
-    BAT : {symbol: "BAT", amount: 6474},
-    BTC : {symbol: "BTC", amount: 4.01},
-    BTG : {symbol: "BTG", amount: 4.01},
-    BTS : {symbol: "BTS", amount: 12500},
-    EOS : {symbol: "EOS", amount: 5860},
-    IOT : {symbol: "IOT", amount: 10200},
-    LTC : {symbol: "LTC", amount: 20.01},
-    TRX : {symbol: "TRX", amount: 6740},
-    XLM : {symbol: "XLM", amount: 1260},
-    XRB : {symbol: "XRB", amount: 100},
-}
-
-// GET API DATA
-// 1. Fetch + JSON + ES6 Literals=> Cryptocompare API
-// Cryptocompare/Price/Multi (returns Object + Multicoin object prices)
-// let cryptosUrl = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,LTC,XLM,EOS,TRX,IOT,BTG,XRB,BTS,BAT,XRB&tsyms=USD";
-let cryptosUrl = "https://www.cryptocompare.com/api/data/coinlist/";
-
-
-
-function getCryptos() {
-    let theResponse;
-    fetch(cryptosUrl)
-        .then(checkStatus) // this is only necessary to show the error if exists
-        .then(blob => blob.json()) // First Promise as a Blob
-        .then((res) => {
-            let theResponse = res;
-            // Visualize Data
-            let blobData = JSON.stringify(theResponse, null, 2);
-            data.innerHTML = blobData;
-        // console.log(blockchainData);
-        }) // Second Promise as real response
-        .catch(err => console.log('There was an error', err)); // In case of Error
-    }
-
-    function checkStatus(res) {
-        if (res.ok){
-            return res;
-        }
-
-        let error = new Error(response.statusText);
-        error.response = response;
-        return Promise.reject(error);
-}
-
-
-
-window.addEventListener('load', function() {
-    console.log('window - load - bubble');
-    getCryptos();
-  });
