1dfb295f1ebcae1e028b014c008da9cd48b3a562
elbernat <elbernat@gmail.com>
2018-04-13 21:46:14 -0400

    variables on dot notation
    Finnally I could use variables in dot notation with success!!!

--- dist/plan.js
+++ dist/plan.js
@@ -1,106 +1,94 @@
 
 // CRYPTO CLASS
 class Crypto {
-    constructor(inputSymbol, inputAmount){
+    constructor(inputSymbol, inputHodl){
         this.symbol = inputSymbol;
-        this.amount = inputAmount;
+        this.hodl = inputHodl;
     }
 }
 
 // ADD CRYPTOS
 let inputSymbol ="";
-let inputAmount = "";
+let inputHodl = "";
 let cryptoAdded = {};
 let cryptosList = [];
 
 function addCrypto() {
     // input data taken ( a string and a number )
     inputSymbol = String(document.getElementById('symbolCrypto').value).toUpperCase(); // string
-    inputAmount = Number(document.getElementById('amountCrypto').value); // number
+    inputHodl = Number(document.getElementById('amountCrypto').value); // number
     // crypto object creation
-    cryptoAdded = new Crypto(inputSymbol, inputAmount);
+    cryptoAdded = new Crypto(inputSymbol, inputHodl);
     // Added to the cryptolist
     cryptosList.push(cryptoAdded);
     // consoles
     console.log(`Crypto added: ${cryptoAdded.symbol}`);
-    console.log(`Amount added: ${cryptoAdded.amount}`);
+    console.log(`Amount added: ${cryptoAdded.hodl}`);
     console.log(cryptoAdded);
     console.log(cryptosList);
     console.log(cryptosList[0]);
+    console.log('=========================');
     // clean inputs
     inputSymbol.value = '';
-    inputAmount.value = '';
+    inputHodl.value = '';
     // get cryptos
     getCryptos(cryptoAdded);
 }
 
 // GET API DATA
-let theResponse;
+
 function getCryptos(cryptoAdded) {
+    let newCrypto = cryptoAdded.symbol;
+    console.log('MY NEW CRYPTO: ' + newCrypto);
+    let theResponse;
     // by default we get the BTC value always + any other crypto
     let cryptosUrl = `https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,${cryptoAdded.symbol}&tsyms=USD`;
     fetch(cryptosUrl)
         .then(blob => blob.json()) 
         .then((res) => {
             let theResponse = res;
-            console.log('THE RESPONSE:' + theResponse.BTC.USD);
+            console.log(theResponse);
+            // ONCE WE HAVE THE RESPONSE
+            console.log('THE RESPONSE BTC: ' + theResponse.BTC.USD);
+            console.log('THE RESPONSE NEWCRYPTO: ' + theResponse[newCrypto].USD);
+            // Create DIV
+            createDivCrypto(cryptoAdded, theResponse);
             // Visualize Data
             let blobData = JSON.stringify(theResponse, null, 2);
             data.innerHTML = blobData;
         }) // Second Promise as real response
         .catch(err => console.log('There was an error', err)); // In case of Error
-
-        // Create DIV
-        createDivCrypto(cryptoAdded, theResponse);
 }
 
-// DIV CREATION CLASS CREATION
-function createDivCrypto (cryptoAdded, theResponse) {
-
-    let cryptoSymbol = cryptoAdded.symbol;
-    console.log( 'CRYPTOSYMBOL: ' + cryptoSymbol);
-    // let cryptoValue = theResponse.cryptoSymbol.USD;
-    // let cryptoHodl = cryptoAdded.amount;
-    // let valueBTC = theResponse.BTC.USD;
-    // let cryptoGainUSD = cryptoValue * cryptoHodl;
-    // let cryptoGainBTC = (cryptoValue * cryptoHodl) / valueBTC;
-
-    // create new div element
-    let div_Symbol = document.createElement('div');
-    // let div_Value = document.createElement('div');
-    // let div_Hodl = document.createElement('div');
-    // let div_GainUSD = document.createElement('div');
-    // let div_GainBTC = document.createElement('div');
+// Create DIV function
+function createDivCrypto(cryptoAdded, theResponse) {
     
-    // add a class
-    div_Symbol.setAttribute('class', 'grid-design fadeIn');
-    // div_Value.setAttribute('class', 'grid-design fadeIn');
-    // div_Hodl.setAttribute('class', 'grid-design fadeIn');
-    // div_GainUSD.setAttribute('class', 'grid-design fadeIn');
-    // div_GainBTC.setAttribute('class', 'grid-design fadeIn');
+    // DIV Selection
+    let myContainer = document.getElementById('cryptos');
 
-    // text crypto content
-    let cryptoSymbolContent = document.createTextNode(cryptoSymbol);
-    // let cryptoValueContent = document.createTextNode(cryptoValue);    
-    // let cryptoHodlContent = document.createTextNode(cryptoHodl);
-    // let cryptoGainUSDContent = document.createTextNode(cryptoGainUSD);
-    // let cryptoGainBTCContent = document.createTextNode(cryptoGainBTC);
-
-    // add text to the div
-    div_Symbol.appendChild(cryptoSymbolContent);
-    // div_Value.appendChild(cryptoHodlContent);
-    // div_Hodl.appendChild(cryptoValueContent);
-    // div_GainUSD.appendChild(cryptoGainUSDContent);
-    // div_GainBTC.appendChild(cryptoGainBTCContent);
-
-    // add the new Element to the DOM
-    let newtDiv = document.getElementById('cryptos');
-    newtDiv.appendChild(div_Symbol);
-    // newtDiv.appendChild(div_Value);
-    // newtDiv.appendChild(div_Hodl);
-    // newtDiv.appendChild(div_GainUSD);
-    // newtDiv.appendChild(div_GainBTC);
-
-    // send a message
-    console.log('new crypto!');
+    // Variables
+    console.log('---------------------')
+    let cryptoSymbol = cryptoAdded.symbol; // OK
+    console.log('SYMBOL: ' + cryptoSymbol); 
+    let cryptoHodl = cryptoAdded.hodl; // OK
+    console.log('HODL: ' + cryptoHodl);
+    let cryptoHodlValue = theResponse[cryptoSymbol].USD; // OK 
+    console.log('HODL VALUE: ' + cryptoHodlValue);
+    let cryptoBTCValue = theResponse.BTC.USD; // OK
+    console.log('BTC VALUE: ' + cryptoBTCValue);
+    let crytpoGainUSD = Math.round(cryptoHodl * cryptoHodlValue); // OK
+    console.log('GAINUSD: ' + crytpoGainUSD);
+    let crytpoGainBTC = Math.round((cryptoHodl * cryptoHodlValue) / cryptoBTCValue); //
+    console.log('GAINBTC: ' + crytpoGainBTC);
+   
+    // BTC
+    let htmlString =
+    `<div>${cryptoSymbol}</div> 
+    <div>${cryptoHodl}</div>
+    <div>${cryptoHodlValue}</div>
+    <div>${cryptoBTCValue}</div>
+    <div>${crytpoGainUSD}</div>
+    <div>${crytpoGainBTC}</div>
+    `
+    myContainer.insertAdjacentHTML('beforeend', htmlString);
 }
